35b8ef40d9f013439432afe76f34ca898cf6169e
Idar <saverfullier@gmail.com>
2024-03-24 17:32:10 +0300

    new goodFunction function. sasukeee aaaaaaaaaa o lovr uuuu aaaaaaaaaaaaaaaaaaa

--- src/script.js
+++ src/script.js
@@ -21,3 +21,9 @@ const lolImTheRapper = () => {
 const suka = () => {
     alert ('vrode poluchilos`!');
 }
+
+const goodFunction = () => {
+    for (let i = 0; i < 9; i++) {
+        alert('I love u Sasuke Uchiha!!!!!');
+    }
+}
